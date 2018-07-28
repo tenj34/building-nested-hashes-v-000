14a9355fa57ee44caeb98c75474106344084009e
Juan Tenezaca <tenj34@gmail.com>
2018-07-28 01:13:32 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -6,14 +6,13 @@ def second_challenge
       :matriarch => {},
       :hero => {},
       :hero_friends => []
-      },
+      }
     :capulet => {
       #your key/value pairs here!
       :patriarch => {},
       :matriarch => {},
       :heroine => {},
-      :heroine_friends => {}
-      }}
-
-
+      :heroine_friends => []
+      }
+    }
 end
