9e4e22b2e3de19deae327d4d30cce773667fbf77
Juan Tenezaca <tenj34@gmail.com>
2018-07-28 01:05:51 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,10 +2,12 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       #your key/value pairs here!
-      }, 
+      :patriarch{
+      }
+      },
     :capulet => {
       #your key/value pairs here!
       }}
 
-  
+
 end
